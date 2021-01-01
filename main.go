266c547e00eb546c3778b930f03c144ff6e44bf9
Kishan Gor <kishan.gor@plus91.in>
2021-01-01 14:22:01 +0530

    Added a dummy function that export a demo file

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "log"
     "net/http"
+    "github.com/360EntSecGroup-Skylar/excelize/v2"
 )
 
 func ping(w http.ResponseWriter, r *http.Request){
@@ -11,10 +12,27 @@ func ping(w http.ResponseWriter, r *http.Request){
 }
 
 func exportExcel(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Exporting Excel!");
+    // Get the Excel file with the user input data
+  file := PrepareDummyAndReturnExcel()
+
+  // Set the headers necessary to get browsers to interpret the downloadable file
+  w.Header().Set("Content-Type", "application/octet-stream")
+  w.Header().Set("Content-Disposition", "attachment;filename=\"demoExcel.xlsx\"")
+  w.Header().Set("File-Name", "demoExcel.xlsx")
+  w.Header().Set("Content-Transfer-Encoding", "binary")
+  w.Header().Set("Expires", "0")
+  file.Write(w)
 }
 
-func exportExcel(w http.ResponseWriter, r *http.Request){
+func PrepareDummyAndReturnExcel() *excelize.File {
+   f := excelize.NewFile()
+   f.SetCellValue("Sheet1", "A1", "Kishan")
+   f.SetCellValue("Sheet1", "A2", "Gor")
+   return f
+}
+
+
+func importExcel(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Importing Excel!");
 }
 
